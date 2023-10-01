5198468257f3186622ec65fc5b8003b8b53a04c3
Jimb Esser <wasteland@gmail.com>
2023-09-30 22:07:26 -0700

    Use production high score list on pink

--- src/glov/client/score.ts
+++ src/glov/client/score.ts
@@ -30,8 +30,8 @@ if (lsd[PLAYER_NAME_KEY]) {
 
 let score_host = 'http://scores.dashingstrike.com';
 if (window.location.host.indexOf('localhost') !== -1 ||
-  window.location.host.indexOf('staging') !== -1 ||
-  window.location.host.indexOf('pink') !== -1
+  window.location.host.indexOf('staging') !== -1/* ||
+  window.location.host.indexOf('pink') !== -1*/
 ) {
   score_host = 'http://scores.staging.dashingstrike.com';
 }
