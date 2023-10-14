e12c1980b74f916216031cccb7f6e1a168d7a958
Jimb Esser <wasteland@gmail.com>
2023-10-14 11:31:07 -0700

    build: include font files by default

--- build/config.project.js
+++ build/config.project.js
@@ -1,10 +1,4 @@
 module.exports = function (config) {
-  // nothing; pure defaults
-  config.client_static.push(
-    'client/**/*.woff',
-    'client/**/*.woff2',
-  );
-
   config.extra_index = [{
     name: 'itch',
     defines: {
